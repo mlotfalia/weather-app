46db5ca8f650f871ec3bba314019997fb36f5e1c
mlotfalia <mahsa.lotfalia@gmail.com>
2021-11-16 22:57:53 +0330

    Add forecasting future temperatures

--- src/app.js
+++ src/app.js
@@ -12,35 +12,87 @@ function showDate() {
   let hour = date.getHours();
   let minute = date.getMinutes();
   if (hour < 10) {
-    hour = "0" + hour;
+    hour = `0${hour}`;
   }
   if (minute < 10) {
-    minute = "0" + minute;
+    minute = `0${minute}`;
   }
   document.querySelector(".day-time").innerHTML = `${
     day[date.getDay()]
   } ${date.getHours()}:${date.getMinutes()}`;
 }
+
+function getForecast(coordinates) {
+  let lat = coordinates.lat;
+  let lon = coordinates.lon;
+  let apiKey = "6b4cc45caef1080d712ecd6ee0009b14";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 function showTemp(response) {
-  console.log(response.data);
   let city = document.querySelector("#city");
   let temp = document.querySelector("#temp");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let description = document.querySelector(".description");
+  let icon = document.querySelector("#weatherIcon");
   centigradeTemp = response.data.main.temp;
   temp.innerHTML = Math.round(centigradeTemp);
   city.innerHTML = response.data.name;
-
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = response.data.wind.speed;
-  document.querySelector(".description").innerHTML =
-    response.data.weather[0].description;
-  let icon = document.querySelector("#weatherIcon");
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = response.data.wind.speed;
+  description.innerHTML = response.data.weather[0].description;
   icon.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   icon.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
   showDate();
 }
+
+function formatDay(times) {
+  let date = new Date(times * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHtml = `<div class ="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHtml =
+        forecastHtml +
+        `  <div class="col-2">
+                    <div class="weather-forecast-date">
+                          ${formatDay(forecastDay.dt)}
+                    </div>
+                      <img                
+                           src=  "https://openweathermap.org/img/wn/${
+                             forecastDay.weather[0].icon
+                           }@2x.png "
+                        alt=""
+                        width = "32"
+                      />
+                      <div class="weather-forecast-temp">
+                      <span class="weather-forecast-temp-max">${Math.round(
+                        forecastDay.temp.max
+                      )}° </span>
+                      <span class="weather-forecast-temp-min">${Math.round(
+                        forecastDay.temp.min
+                      )}°</span>
+                    </div>
+                  </div>
+              `;
+    }
+  });
+  forecastHtml = forecastHtml + `</div>`;
+  forecastElement.innerHTML = forecastHtml;
+}
+
 function convertToF(event) {
   event.preventDefault();
   let temp = document.querySelector("#temp");
